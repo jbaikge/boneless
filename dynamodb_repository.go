6d145ee35917a61155f5811499977f893a39f9aa
jake <bigjake2s@gmail.com>
2022-06-15 16:11:03 -0400

    Filling in GetClassById and UpdateClass

--- dynamodb_repository.go
+++ dynamodb_repository.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 type DynamoDBRepository struct {
@@ -29,6 +30,24 @@ func (r DynamoDBRepository) GetAllClasses(ctx context.Context) (classes []Class,
 }
 
 func (r DynamoDBRepository) GetClassById(ctx context.Context, id string) (class Class, err error) {
+	keyId, err := attributevalue.Marshal(id)
+	if err != nil {
+		return
+	}
+
+	params := &dynamodb.GetItemInput{
+		TableName: &r.table,
+		Key: map[string]types.AttributeValue{
+			"Id": keyId,
+		},
+	}
+
+	response, err := r.client.GetItem(ctx, params)
+	if err != nil {
+		return
+	}
+
+	err = attributevalue.Unmarshal(response.Item, &class)
 	return
 }
 
@@ -48,5 +67,16 @@ func (r DynamoDBRepository) InsertClass(ctx context.Context, class *Class) (err 
 }
 
 func (r DynamoDBRepository) UpdateClass(ctx context.Context, class *Class) (err error) {
+	item, err := attributevalue.MarshalMap(class)
+	if err != nil {
+		return
+	}
+
+	params := &dynamodb.PutItemInput{
+		TableName: &r.table,
+		Item:      item,
+	}
+
+	_, err = r.client.PutItem(ctx, params)
 	return
 }
