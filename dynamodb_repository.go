49402153eb7bdb5eb481f88937b0a5f2b30e5a85
jake <bigjake2s@gmail.com>
2022-06-15 16:06:05 -0400

    Adding context parameters to funcs; filling in InsertClass body

--- dynamodb_repository.go
+++ dynamodb_repository.go
@@ -1,36 +1,52 @@
 package gocms
 
 import (
+	"context"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
-var _ Repository = new(DynamoDBRepository)
-
 type DynamoDBRepository struct {
 	client *dynamodb.Client
+	table  string
 }
 
-func NewDynamoDBRepository(config aws.Config) Repository {
-	return &DynamoDBRepository{}
+func NewDynamoDBRepository(config aws.Config, table string) Repository {
+	return &DynamoDBRepository{
+		client: dynamodb.NewFromConfig(config),
+		table:  table,
+	}
 }
 
-func (r DynamoDBRepository) DeleteClass(id string) (err error) {
+func (r DynamoDBRepository) DeleteClass(ctx context.Context, id string) (err error) {
 	return
 }
 
-func (r DynamoDBRepository) GetAllClasses() (classes []Class, err error) {
+func (r DynamoDBRepository) GetAllClasses(ctx context.Context) (classes []Class, err error) {
 	return
 }
 
-func (r DynamoDBRepository) GetClassById(id string) (class Class, err error) {
+func (r DynamoDBRepository) GetClassById(ctx context.Context, id string) (class Class, err error) {
 	return
 }
 
-func (r DynamoDBRepository) InsertClass(class *Class) (err error) {
+func (r DynamoDBRepository) InsertClass(ctx context.Context, class *Class) (err error) {
+	item, err := attributevalue.MarshalMap(class)
+	if err != nil {
+		return
+	}
+
+	params := &dynamodb.PutItemInput{
+		TableName: &r.table,
+		Item:      item,
+	}
+
+	_, err = r.client.PutItem(ctx, params)
 	return
 }
 
-func (r DynamoDBRepository) UpdateClass(class *Class) (err error) {
+func (r DynamoDBRepository) UpdateClass(ctx context.Context, class *Class) (err error) {
 	return
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/aws/aws-lambda-go v1.32.0
 	github.com/aws/aws-sdk-go-v2 v1.16.5
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.9.3
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.15.6
 	github.com/rs/xid v1.4.0
 )
@@ -12,6 +13,7 @@ require (
 require (
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.12 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.13.6 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.6 // indirect
 	github.com/aws/smithy-go v1.11.3 // indirect
