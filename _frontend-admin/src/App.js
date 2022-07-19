02b228c6fbf43190fe164fc521d23ce03cf415a3
jake <bigjake2s@gmail.com>
2022-07-19 08:49:57 -0400

    Using ra-data-simple-rest for REST communication

--- _frontend-admin/src/App.js
+++ _frontend-admin/src/App.js
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import { Admin, Resource } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
 import { ClassCreate, ClassEdit, ClassList } from './classes';
-import dataProvider from './dataProvider';
+// import dataProvider from './dataProvider';
 import { darkTheme } from './theme';
 
-// import jsonServerProvider from 'ra-data-json-server';
-// const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-
 const App = () => (
-  <Admin dataProvider={dataProvider} theme={darkTheme}>
+  <Admin dataProvider={simpleRestProvider(window._env_.API_URL)} theme={darkTheme}>
     <Resource name="classes" create={ClassCreate} edit={ClassEdit} list={ClassList} />
   </Admin>
 );
