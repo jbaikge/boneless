71870646cb1ded052c3628e10fcf9014503412f5
jake <bigjake2s@gmail.com>
2022-07-20 14:45:43 -0400

    Refactored to put processing inside sub-func

--- lambda/list-classes/list-classes.go
+++ lambda/list-classes/list-classes.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -14,40 +15,63 @@ import (
 	"github.com/jbaikge/gocms"
 )
 
+const RangeKey = "classes"
+
 var (
 	dynamoConfig aws.Config
 	dynamoTables gocms.DynamoDBTables
 )
 
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (response events.APIGatewayProxyResponse, err error) {
-	fmt.Println("Headers:")
-	for key, value := range request.Headers {
-		fmt.Printf("    %s: %s\n", key, value)
-	}
-
-	filter := gocms.ClassFilter{
-		Range: gocms.Range{},
-	}
-
-	repo := gocms.NewDynamoDBRepository(dynamoConfig, dynamoTables)
-	service := gocms.NewClassService(repo)
-
-	list, r, err := service.List(context.Background(), filter)
-	if err != nil {
-		return
-	}
-
-	body, err := json.Marshal(&list)
-	if err != nil {
-		return
-	}
-
 	response.Headers = map[string]string{
-		"Content-Type":                "application/json",
-		"Access-Control-Allow-Origin": "*",
-		"Content-Range":               r.ContentRangeHeader("classes"),
+		"Content-Type":                 "application/json",
+		"Access-Control-Allow-Headers": "Content-Range",
+		"Access-Control-Allow-Origin":  "*",
 	}
 	response.StatusCode = http.StatusOK
+
+	out, err := func() (out interface{}, err error) {
+		filter := gocms.ClassFilter{
+			Range: gocms.Range{
+				Start: 0,
+				End:   10,
+			},
+		}
+
+		rangeHeader, ok := request.Headers["range"]
+		if ok && strings.HasPrefix(rangeHeader, RangeKey) {
+			_, err = fmt.Scanf(RangeKey+"=%d-%d", &filter.Range.Start, &filter.Range.End)
+			if err != nil {
+				err = fmt.Errorf("bad range header: %s - %v", rangeHeader, err)
+				return
+			}
+		}
+
+		repo := gocms.NewDynamoDBRepository(dynamoConfig, dynamoTables)
+		service := gocms.NewClassService(repo)
+
+		list, r, err := service.List(context.Background(), filter)
+		if err != nil {
+			return
+		}
+
+		response.Headers["Content-Range"] = r.ContentRangeHeader(RangeKey)
+		return list, nil
+	}()
+
+	if err != nil {
+		out = struct {
+			Error string `json:"error"`
+		}{
+			Error: err.Error(),
+		}
+	}
+
+	body, err := json.Marshal(out)
+	if err != nil {
+		return
+	}
+
 	response.Body = string(body)
 	return
 }
