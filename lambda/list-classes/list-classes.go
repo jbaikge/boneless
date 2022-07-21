ff6139141e8188ddb43cbc40b10a7bb1256ecb53
jake <bigjake2s@gmail.com>
2022-07-21 09:39:54 -0400

    Trying to process range headers

--- lambda/list-classes/list-classes.go
+++ lambda/list-classes/list-classes.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -25,6 +25,7 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	response.Headers = map[string]string{
 		"Content-Type":                  "application/json",
 		"Access-Control-Expose-Headers": "Content-Range",
+		"Access-Control-Allow-Headers":  "Range",
 		"Access-Control-Allow-Origin":   "*",
 	}
 	response.StatusCode = http.StatusOK
@@ -32,30 +33,37 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	out, err := func() (out interface{}, err error) {
 		filter := gocms.ClassFilter{
 			Range: gocms.Range{
-				Start: 0,
-				End:   10,
+				End: 9,
 			},
 		}
 
 		rangeHeader, ok := request.Headers["range"]
-		if ok && strings.HasPrefix(rangeHeader, RangeUnit) {
+		if !ok {
+			rangeHeader, ok = request.Headers["Range"]
+			fmt.Printf("Capitalized ")
+		}
+		fmt.Printf("Range Header: %v\n", rangeHeader)
+
+		if ok {
 			if err = filter.Range.ParseHeader(rangeHeader, RangeUnit); err != nil {
 				response.StatusCode = http.StatusRequestedRangeNotSatisfiable
 				return
 			}
 		}
+		fmt.Printf("Range From Header: %+v\n", filter.Range)
 
-		rangeParam, ok := request.QueryStringParameters["range"]
-		if filter.Range.IsZero() && ok {
-			bounds := make([]int, 0, 2)
-			err = json.Unmarshal([]byte(rangeParam), &bounds)
-			if err != nil || len(bounds) != 2 {
-				response.StatusCode = http.StatusRequestedRangeNotSatisfiable
-				return
-			}
-			filter.Range.Start = bounds[0]
-			filter.Range.End = bounds[1]
-		}
+		// rangeParam, ok := request.QueryStringParameters["range"]
+		// if filter.Range.IsZero() && ok {
+		// 	bounds := make([]int, 0, 2)
+		// 	err = json.Unmarshal([]byte(rangeParam), &bounds)
+		// 	if err != nil || len(bounds) != 2 {
+		// 		response.StatusCode = http.StatusRequestedRangeNotSatisfiable
+		// 		return
+		// 	}
+		// 	filter.Range.Start = bounds[0]
+		// 	filter.Range.End = bounds[1]
+		// }
+		// fmt.Printf("Range From Param: %+v\n", filter.Range)
 
 		repo := gocms.NewDynamoDBRepository(dynamoConfig, dynamoTables)
 		service := gocms.NewClassService(repo)
