1ce438dd161324d4f028c72b5acdeb5711e51227
jake <bigjake2s@gmail.com>
2022-07-22 09:18:05 -0400

    Adding parameter parsing for different REST API data provdiers

--- range.go
+++ range.go
@@ -43,16 +43,98 @@ func (r *Range) ParseHeader(header, unit string) (err error) {
 	}
 
 	if r.Start, err = strconv.Atoi(start); err != nil {
-		return fmt.Errorf("malformed start value (%s): %v", start, err)
+		return fmt.Errorf("malformed start value (%s): %w", start, err)
 	}
 
 	if r.End, err = strconv.Atoi(end); err != nil {
-		return fmt.Errorf("malformed end value (%s): %v", end, err)
+		return fmt.Errorf("malformed end value (%s): %w", end, err)
 	}
 
 	if r.End < r.Start {
 		return fmt.Errorf("invalid range, end before start (%d < %d)", r.End, r.Start)
 	}
 
+	return
+}
+
+// Supports two different combinations of parameters:
+// _start, _end: Zero-based indexes for the starting and ending items
+// _page, _per_page: paging with _page starting at 1; _per_page defaults to 10
+// If no useful parameters are available, defaults to the first 10 items
+func (r *Range) ParseParams(params map[string]string) (err error) {
+	var start, end, page, perPage int
+
+	startStr, hasStart := params["_start"]
+	endStr, hasEnd := params["_end"]
+	pageStr, hasPage := params["_page"]
+	perPageStr, hasPerPage := params["_per_page"]
+
+	if hasStart && hasEnd && hasPage && hasPerPage {
+		return fmt.Errorf("too many paging parameters; choose _start, _end or _page, _per_page")
+	}
+
+	if hasStart && !hasEnd {
+		return fmt.Errorf("missing _end with _start")
+	}
+
+	if !hasStart && hasEnd {
+		return fmt.Errorf("missing _start with _end")
+	}
+
+	if !hasPage && hasPerPage {
+		page = 1
+	}
+
+	if hasPage && !hasPerPage {
+		perPage = 10
+	}
+
+	if hasStart && hasEnd {
+		if start, err = strconv.Atoi(startStr); err != nil {
+			return fmt.Errorf("parsing _start: %w", err)
+		}
+		if end, err = strconv.Atoi(endStr); err != nil {
+			return fmt.Errorf("parsing _end: %w", err)
+		}
+		if start < 0 {
+			return fmt.Errorf("_start is less than zero")
+		}
+		if end < 0 {
+			return fmt.Errorf("_end is less than zero")
+		}
+		if start > end {
+			return fmt.Errorf("_start is greater than _end")
+		}
+
+		r.Start, r.End = start, end
+		return
+	}
+
+	if hasPage {
+		if page, err = strconv.Atoi(pageStr); err != nil {
+			return fmt.Errorf("parsing _page: %w", err)
+		}
+		if page < 1 {
+			return fmt.Errorf("_page is less than one")
+		}
+	}
+
+	if hasPerPage {
+		if perPage, err = strconv.Atoi(perPageStr); err != nil {
+			return fmt.Errorf("parsing _per_page: %w", err)
+		}
+		if perPage < 1 {
+			return fmt.Errorf("_per_page is less than one")
+		}
+	}
+
+	if hasPage || hasPerPage {
+		r.Start = (page - 1) * perPage
+		r.End = page*perPage - 1
+		return
+	}
+
+	// Default to 10 items on page 1
+	r.End = 9
 	return
 }
--- range_test.go
+++ range_test.go
@@ -68,3 +68,132 @@ func TestParseHeader(t *testing.T) {
 		assert.Error(t, r.ParseHeader(unit+"=9-0", unit))
 	})
 }
+
+func TestParseParams(t *testing.T) {
+	tests := []struct {
+		Name   string
+		Params map[string]string
+		Start  int
+		End    int
+		Error  bool
+	}{
+		{
+			Name:   "Empty",
+			Params: map[string]string{},
+			Start:  0,
+			End:    9,
+		},
+		// React Admin data provider: ra-data-json-server
+		{
+			Name:   "StartOnly",
+			Params: map[string]string{"_start": "10"},
+			Error:  true,
+		},
+		{
+			Name:   "EndOnly",
+			Params: map[string]string{"_end": "19"},
+			Error:  true,
+		},
+		{
+			Name:   "StartAndEnd",
+			Params: map[string]string{"_start": "10", "_end": "19"},
+			Start:  10,
+			End:    19,
+		},
+		{
+			Name:   "StartGreaterEnd",
+			Params: map[string]string{"_start": "19", "_end": "10"},
+			Error:  true,
+		},
+		{
+			Name:   "NegativeStart",
+			Params: map[string]string{"_start": "-5", "_end": "5"},
+			Error:  true,
+		},
+		{
+			Name:   "NegativeEnd",
+			Params: map[string]string{"_start": "5", "_end": "-5"},
+			Error:  true,
+		},
+		{
+			Name:   "InvalidStartInt",
+			Params: map[string]string{"_start": "zero", "_end": "4"},
+			Error:  true,
+		},
+		{
+			Name:   "InvalidEndInt",
+			Params: map[string]string{"_start": "0", "_end": "four"},
+			Error:  true,
+		},
+		// page/per-page
+		{
+			Name:   "PageOnly",
+			Params: map[string]string{"_page": "4"},
+			Start:  30,
+			End:    39,
+		},
+		{
+			Name:   "PerPageOnly",
+			Params: map[string]string{"_per_page": "5"},
+			Start:  0,
+			End:    4,
+		},
+		{
+			Name:   "PageAndPerPage",
+			Params: map[string]string{"_page": "5", "_per_page": "5"},
+			Start:  20,
+			End:    24,
+		},
+		{
+			Name:   "NegativePage",
+			Params: map[string]string{"_page": "-1", "_per_page": "10"},
+			Error:  true,
+		},
+		{
+			Name:   "NegativePerPage",
+			Params: map[string]string{"_page": "5", "_per_page": "-1"},
+			Error:  true,
+		},
+		{
+			Name:   "ZeroPerPage",
+			Params: map[string]string{"_page": "5", "_per_page": "0"},
+			Error:  true,
+		},
+		{
+			Name:   "InvalidPageInt",
+			Params: map[string]string{"_page": "one", "_per_page": "5"},
+			Error:  true,
+		},
+		{
+			Name:   "InvalidPerPageInt",
+			Params: map[string]string{"_page": "1", "_per_page": "five"},
+			Error:  true,
+		},
+		// Cause chaos
+		{
+			Name: "Chaos",
+			Params: map[string]string{
+				"_start":    "0",
+				"_end":      "9",
+				"_page":     "5",
+				"_per_page": "5",
+			},
+			Error: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			var r Range
+			err := r.ParseParams(test.Params)
+			if test.Error {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, test.Start, r.Start)
+			assert.Equal(t, test.End, r.End)
+		})
+	}
+}
