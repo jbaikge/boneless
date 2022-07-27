0ea23c90495a1850b70a1efc66dfa61f7f4c1f13
jake <bigjake2s@gmail.com>
2022-07-27 15:03:15 -0400

    Dynamically loading in main resources

--- _frontend-admin/src/App.js
+++ _frontend-admin/src/App.js
@@ -5,9 +5,15 @@ import { ClassCreate, ClassEdit, ClassList } from './classes';
 // import dataProvider from './dataProvider';
 import { darkTheme } from './theme';
 
+const fetchResources = () =>
+  fetch(window._env_.API_URL + '/classes?_start=0&_end=50')
+    .then(response => response.json())
+    .then(classes => classes.map(c => <Resource options={{ label: c.name }} name={"classes/" + c.id + "/documents"} key={c.id} list={ClassList} />));
+
 const App = () => (
   <Admin dataProvider={simpleRestProvider(window._env_.API_URL)} theme={darkTheme}>
     <Resource name="classes" create={ClassCreate} edit={ClassEdit} list={ClassList} />
+    {fetchResources}
   </Admin>
 );
 
