bed128b5a65a9319cbe9f3b849d8b20de6bd2a45
jake <bigjake2s@gmail.com>
2022-07-28 14:59:39 -0400

    Adding templates resource; organizing fetchResources

--- _frontend-admin/src/App.js
+++ _frontend-admin/src/App.js
@@ -8,12 +8,18 @@ import { darkTheme } from './theme';
 const fetchResources = () =>
   fetch(window._env_.API_URL + '/classes?_start=0&_end=50')
     .then(response => response.json())
-    .then(classes => classes.map(c => <Resource options={{ label: c.name }} name={"classes/" + c.id + "/documents"} key={c.id} list={ClassList} />));
+    .then(classes => classes.map(c => <Resource
+      options={{ label: c.name }}
+      name={"classes/" + c.id + "/documents"}
+      key={c.id}
+      list={ClassList}
+    />));
 
 const App = () => (
   <Admin dataProvider={simpleRestProvider(window._env_.API_URL)} theme={darkTheme}>
-    <Resource name="classes" create={ClassCreate} edit={ClassEdit} list={ClassList} />
     {fetchResources}
+    <Resource name="classes" options={{ label: 'Manage Classes' }} create={ClassCreate} edit={ClassEdit} list={ClassList} />
+    <Resource name="templates" list={ClassList} />
   </Admin>
 );
 
