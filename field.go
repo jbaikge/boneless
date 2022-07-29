d8621d8db2033f0bf1bfdb675cbb3765e172fa37
jake <bigjake2s@gmail.com>
2022-07-29 19:48:06 -0400

    Adding Sort field to Field struct

--- field.go
+++ field.go
@@ -4,6 +4,7 @@ type Field struct {
 	Type    string `json:"type"`
 	Name    string `json:"name"`
 	Label   string `json:"label"`
+	Sort    bool   `json:"sort"`
 	Min     string `json:"min"`
 	Max     string `json:"max"`
 	Step    string `json:"step"`
