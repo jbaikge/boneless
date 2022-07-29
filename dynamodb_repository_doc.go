b833de6cd78642b80135cf22b1b1c3b8afe116a4
jake <bigjake2s@gmail.com>
2022-07-29 09:27:50 -0400

    Adding TemplateId and VersionId

--- dynamodb_repository_doc.go
+++ dynamodb_repository_doc.go
@@ -6,6 +6,8 @@ type dynamoDocument struct {
 	DocumentId string
 	ClassId    string
 	ParentId   string
+	TemplateId string
+	VersionId  string
 	Title      string
 	Version    int
 	Created    time.Time
@@ -16,6 +18,7 @@ func (dyn dynamoDocument) FromDocument(d *Document) {
 	dyn.DocumentId = d.Id
 	dyn.ClassId = d.ClassId
 	dyn.ParentId = d.ParentId
+	dyn.TemplateId = d.TemplateId
 	dyn.Title = d.Title
 	dyn.Version = d.Version
 	dyn.Created = d.Created
@@ -26,6 +29,7 @@ func (dyn dynamoDocument) ToDocument() (d Document) {
 	d.Id = dyn.DocumentId
 	d.ClassId = dyn.ClassId
 	d.ParentId = dyn.ParentId
+	d.TemplateId = dyn.TemplateId
 	d.Title = dyn.Title
 	d.Version = dyn.Version
 	d.Created = dyn.Created
