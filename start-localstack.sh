d92ee1e55c7da17fa6454aad44ee4a03b9bb7ce0
jake <bigjake2s@gmail.com>
2022-08-09 11:47:08 -0400

    Including host for host.docker.internal

--- start-localstack.sh
+++ start-localstack.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -it --publish 4566:4566 --publish 4510-4559:4510-4559 localstack/localstack:1.0.0
+docker run --rm --add-host host.docker.internal:host-gateway --publish 4566:4566 --publish 4510-4559:4510-4559 localstack/localstack:1.0.0
