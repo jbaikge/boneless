e8c4d3b8c6b70e8f7ae58ffed174f90c86501eaf
jake <bigjake2s@gmail.com>
2022-08-11 13:59:49 -0400

    First round of handlers for creating a class

--- lambda/admin/main.go
+++ lambda/admin/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
-	"os"
+	"net/http"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,20 +16,48 @@ import (
 	"github.com/jbaikge/gocms"
 )
 
+type Error struct {
+	Error string `json:"error"`
+}
+
 var (
-	lambdaConfig aws.Config
-	resources    gocms.DynamoDBResources
+	awsConfig aws.Config
+	resources gocms.DynamoDBResources
 )
 
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (response events.APIGatewayProxyResponse, err error) {
-	enc := json.NewEncoder(os.Stdout)
-	err = enc.Encode(request)
+	response.StatusCode = http.StatusOK
+	response.Headers = map[string]string{
+		"Content-Type": "application/json",
+	}
+
+	repo := gocms.NewDynamoDBRepository(awsConfig, resources)
+
+	var data interface{}
+	switch fmt.Sprintf("%s %s", request.HTTPMethod, request.Resource) {
+	case "GET /classes":
+		data, err = handleClassList(ctx, request, repo)
+	case "POST /classes":
+		data, err = handleClassCreate(ctx, request, repo)
+	}
+
+	// Redirect the error so it comes out as JSON instead of a 500
+	if err != nil {
+		response.StatusCode = http.StatusBadRequest
+		data = Error{Error: err.Error()}
+	}
+
+	var buffer bytes.Buffer
+	if err = json.NewEncoder(&buffer).Encode(data); err != nil {
+		return
+	}
+	response.Body = buffer.String()
 	return
 }
 
 func main() {
 	var err error
-	lambdaConfig, err = config.LoadDefaultConfig(context.Background())
+	awsConfig, err = config.LoadDefaultConfig(context.Background())
 	if err != nil {
 		log.Fatalf("Failed to load default config: %v", err)
 	}
@@ -34,4 +65,23 @@ func main() {
 	resources.FromEnv()
 
 	lambda.Start(HandleRequest)
+}
+
+//
+// Handlers
+//
+
+func handleClassCreate(ctx context.Context, request events.APIGatewayProxyRequest, repo gocms.Repository) (class gocms.Class, err error) {
+	reader := strings.NewReader(request.Body)
+	if err = json.NewDecoder(reader).Decode(&class); err != nil {
+		return
+	}
+
+	classService := gocms.NewClassService(repo)
+	err = classService.Create(ctx, &class)
+	return
+}
+
+func handleClassList(ctx context.Context, request events.APIGatewayProxyRequest, repo gocms.Repository) (classes []gocms.Class, err error) {
+	return
 }
