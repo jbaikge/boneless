dca1c908d5a77a13b899956a95645b0b527d6260
jake <bigjake2s@gmail.com>
2022-08-16 10:09:57 -0400

    Utilizing useEffect to only render the side navigation initially

--- _frontend-admin/src/App.js
+++ _frontend-admin/src/App.js
@@ -8,24 +8,26 @@ import { DocumentCreate, DocumentEdit, DocumentList } from './documents';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-const fetchResources = () =>
-  fetch(API_URL + '/classes?_start=0&_end=50')
-    .then(response => response.json())
-    .then(classes => classes.map(c => <Resource
-      options={{ label: c.name }}
-      name={"classes/" + c.id + "/documents"}
-      key={c.id}
-      create={DocumentCreate}
-      edit={DocumentEdit}
-      list={DocumentList}
-    />));
+const App = () => {
+  const [resources, setResources] = React.useState([]);
 
-const App = () => (
-  <Admin dataProvider={simpleRestProvider(API_URL)} theme={darkTheme}>
-    {fetchResources}
-    <Resource name="classes" options={{ label: 'Manage Classes' }} create={ClassCreate} edit={ClassEdit} list={ClassList} />
-    <Resource name="templates" list={ClassList} />
-  </Admin>
-);
+  React.useEffect(() => {
+    const fetchClasses = async () => {
+      const response = await fetch(API_URL + '/classes?_start=0&_end=50');
+      const data = await response.json();
+      const classes = data.map(c => <Resource options={{ label: c.name }} key={c.id} name={"classes/" + c.id + "/documents"} create={DocumentCreate} edit={DocumentEdit} list={DocumentList} />);
+      setResources(classes);
+    };
+    fetchClasses();
+  }, []);
+
+  return (
+    <Admin dataProvider={simpleRestProvider(API_URL)} theme={darkTheme}>
+      {resources}
+      <Resource name="classes" options={{ label: 'Manage Classes' }} create={ClassCreate} edit={ClassEdit} list={ClassList} />
+      <Resource name="templates" list={ClassList} />
+    </Admin>
+  );
+};
 
 export default App;
