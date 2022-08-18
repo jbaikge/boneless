956e821720033475f99b483b1ee69a65d635439b
jake <bigjake2s@gmail.com>
2022-08-18 14:04:46 -0400

    Shortening some Resource lines; Using REST provider in useEffect

--- _frontend-admin/src/App.js
+++ _frontend-admin/src/App.js
@@ -15,9 +15,20 @@ const App = () => {
 
   React.useEffect(() => {
     const fetchClasses = async () => {
-      const response = await fetch(API_URL + '/classes?_start=0&_end=50');
-      const data = await response.json();
-      const classes = data.map(c => <Resource options={{ label: c.name }} key={c.id} name={"classes/" + c.id + "/documents"} create={DocumentCreate} edit={DocumentEdit} list={DocumentList} />);
+      const dataProvider = simpleRestProvider(API_URL);
+      const list = await dataProvider.getList('classes', {
+        filter: '',
+        pagination: {page: 1, perPage: 50},
+        sort: {field: 'name', order: 'ASC'},
+      });
+      const classes = list.data.map(c => <Resource
+        options={{ label: c.name }}
+        key={c.id}
+        name={"classes/" + c.id + "/documents"}
+        create={DocumentCreate}
+        edit={DocumentEdit}
+        list={DocumentList}
+      />);
       setResources(classes);
     };
     fetchClasses();
@@ -26,7 +37,13 @@ const App = () => {
   return (
     <Admin dataProvider={simpleRestProvider(API_URL)} theme={prefersDark ? darkTheme : lightTheme}>
       {resources}
-      <Resource name="classes" options={{ label: 'Manage Classes' }} create={<ClassCreate update={setUpdateResources} />} edit={<ClassEdit update={setUpdateResources} />} list={ClassList} />
+      <Resource
+        name="classes"
+        options={{ label: 'Manage Classes' }}
+        create={<ClassCreate update={setUpdateResources} />}
+        edit={<ClassEdit update={setUpdateResources} />}
+        list={ClassList}
+      />
       <Resource name="templates" list={ClassList} />
     </Admin>
   );
