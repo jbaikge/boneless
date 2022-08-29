054e882ec68ca9936de16d6a1c1c37c23d8c11c7
jake <bigjake2s@gmail.com>
2022-08-29 11:29:45 -0400

    Removing "Frontend" prefix from components; Cleaning up dir path

--- _stack/lib/admin-stack.ts
+++ _stack/lib/admin-stack.ts
@@ -11,13 +11,11 @@ import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
 export class AdminStack extends cdk.Stack {
   constructor(scope: constructs.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    // Path back to repo root
-    const rootDir = path.resolve(__dirname, '..', '..');
 
     // Admin frontend
     // Ref: https://aws-cdk.com/deploying-a-static-website-using-s3-and-cloudfront/
     // Ref: https://github.com/aws-samples/cdk-build-bundle-deploy-example/blob/main/cdk-bundle-static-site-example/lib/static-site-stack.ts
-    const adminBucket = new s3.Bucket(this, 'FrontendAdminBucket', {
+    const adminBucket = new s3.Bucket(this, 'AdminBucket', {
       bucketName: cdk.PhysicalName.GENERATE_IF_NEEDED,
       encryption: s3.BucketEncryption.S3_MANAGED,
       accessControl: s3.BucketAccessControl.PRIVATE,
@@ -26,22 +24,23 @@ export class AdminStack extends cdk.Stack {
       autoDeleteObjects: true,
     });
 
-    const adminOriginAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'FrontendAdminOAI');
+    const adminOriginAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'AdminOAI');
     adminBucket.grantRead(adminOriginAccessIdentity);
 
-    const distribution = new cloudfront.Distribution(this, 'FrontendAdminDistribution', {
+    const distribution = new cloudfront.Distribution(this, 'AdminDistribution', {
       defaultRootObject: 'index.html',
       defaultBehavior: {
         origin: new cloudfrontOrigins.S3Origin(adminBucket, {originAccessIdentity: adminOriginAccessIdentity}),
       }
     });
 
-    new s3Deployment.BucketDeployment(this, 'FrontendAdminDeployment', {
+    const buildDir = path.resolve(__dirname, '..', '..', '_frontend-admin', 'build');
+    new s3Deployment.BucketDeployment(this, 'AdminDeployment', {
       destinationBucket: adminBucket,
       distribution: distribution,
       memoryLimit: 128,
       sources: [
-        s3Deployment.Source.asset(path.join(rootDir, '_frontend-admin', 'build')),
+        s3Deployment.Source.asset(buildDir),
       ],
     });
 
