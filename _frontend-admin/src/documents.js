6c3bf5a7757a0e4f07515c15270830656f59dd68
jake <bigjake2s@gmail.com>
2022-09-02 13:19:27 -0400

    Reworking document form with parent selector when defined by the class

--- _frontend-admin/src/documents.js
+++ _frontend-admin/src/documents.js
@@ -20,31 +20,23 @@ import { TinyInput } from './tinyInput';
 
 const resourceRE = /([^/]+)\/([^/]+)\/.*/;
 
-export const DocumentCreate = (props) => (
-  <Create {...props}>
-    <SimpleForm>
-      <TextInput source="path" fullWidth />
-      <ReferenceInput source="template_id" reference="templates" perPage={100}>
-        <SelectInput fullWidth />
-      </ReferenceInput>
-      <DocumentInputs />
-    </SimpleForm>
-  </Create>
-);
+export const DocumentCreate = (props) => {
+  return (
+    <Create {...props}>
+      <DocumentForm />
+    </Create>
+  );
+};
 
-export const DocumentEdit = (props) => (
-  <Edit {...props}>
-    <SimpleForm>
-      <TextInput source="path" fullWidth />
-      <ReferenceInput source="template_id" reference="templates" perPage={100}>
-        <SelectInput fullWidth />
-      </ReferenceInput>
-      <DocumentInputs />
-    </SimpleForm>
-  </Edit>
-);
+export const DocumentEdit = (props) => {
+  return (
+    <Edit {...props}>
+      <DocumentForm />
+    </Edit>
+  );
+};
 
-export const DocumentInputs = () => {
+export const DocumentForm = () => {
   const resourceContext = useResourceContext();
   // resourceContext should be "classes/<id>/documents"
   const [ , resource, id ] = resourceRE.exec(resourceContext);
@@ -54,30 +46,46 @@ export const DocumentInputs = () => {
     return <Loading />;
   }
 
-  return data.fields.map(field => {
-    const source = `values.${field.name}`;
-    switch (field.type) {
-      case 'datetime':
-        return <DateTimeInput key={field.name} source={source} label={field.label} inputProps={{ min: field.min, max: field.max, step: field.step }} />
-      case 'richtext':
-        return <RichTextInput key={field.name} source={source} label={field.label} fullWidth />
-      case 'select-class':
-        return (
-          <ReferenceInput reference={'/classes/' + field.class_id + '/documents'} source={source}>
-            <SelectInput optionText={'values.' + field.field} label={field.label} fullWidth />
-          </ReferenceInput>
-        );
-      case 'text':
-        return <TextInput key={field.name} source={source} label={field.label} fullWidth />
-      case 'time':
-        return <TextInput type="time" key={field.name} source={source} label={field.label} fullWidth />
-      case 'tiny':
-        return <TinyInput key={field.name} source={source} label={field.label} fullWidth />
-      default:
-        return <TextInput key={field.name} source={source} label={`Unknown type (${field.type}) - ${field.label}`} fullWidth />
-    }
-  });
-}
+  let parentInput;
+  if (data.parent_id != "") {
+    parentInput = <ReferenceInput source="parent_id" reference={'classes/' + data.parent_id + '/documents'} perPage={25}>
+      <SelectInput optionText="values.title" fullWidth />
+    </ReferenceInput>;
+  }
+
+  return (
+    <SimpleForm>
+      <TextInput source="path" fullWidth />
+      {parentInput}
+      <ReferenceInput source="template_id" reference="templates" perPage={100}>
+        <SelectInput fullWidth />
+      </ReferenceInput>
+      {data.fields.map(field => {
+        const source = `values.${field.name}`;
+        switch (field.type) {
+          case 'datetime':
+            return <DateTimeInput key={field.name} source={source} label={field.label} inputProps={{ min: field.min, max: field.max, step: field.step }} />
+          case 'richtext':
+            return <RichTextInput key={field.name} source={source} label={field.label} fullWidth />
+          case 'select-class':
+            return (
+              <ReferenceInput reference={'/classes/' + field.class_id + '/documents'} source={source}>
+                <SelectInput optionText={'values.' + field.field} label={field.label} fullWidth />
+              </ReferenceInput>
+            );
+          case 'text':
+            return <TextInput key={field.name} source={source} label={field.label} fullWidth />
+          case 'time':
+            return <TextInput type="time" key={field.name} source={source} label={field.label} fullWidth />
+          case 'tiny':
+            return <TinyInput key={field.name} source={source} label={field.label} fullWidth />
+          default:
+            return <TextInput key={field.name} source={source} label={`Unknown type (${field.type}) - ${field.label}`} fullWidth />
+        }
+      })}
+    </SimpleForm>
+  );
+};
 
 export const DocumentList = (props) => {
   const resourceContext = useResourceContext();
@@ -90,7 +98,7 @@ export const DocumentList = (props) => {
 
   return (
     <List {...props}>
-      <Datagrid rowClick="edit">
+      <Datagrid>
         {data.fields.filter(field => field.column > 0).sort((a, b) => a.column - b.column).map(field => {
           const source = `values.${field.name}`;
           switch (field.type) {
