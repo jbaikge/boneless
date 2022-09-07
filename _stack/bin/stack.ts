41b041dcb9007354107e789b3412b7e29789b969
jake <bigjake2s@gmail.com>
2022-09-07 11:48:56 -0400

    Adding StaticStack and filling in dependencies

--- _stack/bin/stack.ts
+++ _stack/bin/stack.ts
@@ -5,21 +5,26 @@ import { DatabaseStack } from '../lib/database-stack';
 import { ApiStack } from '../lib/api-stack';
 import { FrontendStack } from '../lib/frontend-stack';
 import { AdminStack } from '../lib/admin-stack';
-// import { AdminStack } from '../lib/admin-stack';
-// import { RepositoryStack } from '../lib/repository-stack';
+import { StaticStack } from '../lib/static-stack';
 
 const app = new cdk.App();
 
 const databaseStack = new DatabaseStack(app, 'DatabaseStack', {});
 
-new ApiStack(app, 'ApiStack', {
-    db:     databaseStack.db,
-    bucket: databaseStack.bucket,
+const staticStack = new StaticStack(app, 'StaticStack', {});
+
+const apiStack = new ApiStack(app, 'ApiStack', {
+  db:     databaseStack.db,
+  bucket: databaseStack.bucket,
+  static: staticStack.bucket,
+});
+
+new AdminStack(app, 'AdminStack', {
+  api: apiStack.api,
 });
 
 new FrontendStack(app, 'FrontendStack', {
     db:     databaseStack.db,
     bucket: databaseStack.bucket,
-});
-
-new AdminStack(app, 'AdminStack', {});
+    static: staticStack.bucket,
+  });
