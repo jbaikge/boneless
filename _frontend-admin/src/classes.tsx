fa0aea01197fb745d59dea177cda0b44b03b6c71
jake <bigjake2s@gmail.com>
2022-09-15 19:03:25 -0400

    Adding JSON import processing

--- _frontend-admin/src/classes.tsx
+++ _frontend-admin/src/classes.tsx
@@ -26,7 +26,14 @@ import {
   required,
   useRedirect,
   regex,
+  Title,
+  FileInput,
+  FileField,
+  useDataProvider,
+  CreateResult,
 } from 'react-admin';
+import { FieldValues } from 'react-hook-form';
+import { Card, CardContent } from '@mui/material';
 import { FieldChoices, FieldProps } from './field';
 import './App.css';
 
@@ -177,3 +184,62 @@ export const ClassList = (props: ListProps) => (
     </Datagrid>
   </List>
 );
+
+export const ClassImport = (props: UpdateProps) => {
+  const dataProvider = useDataProvider();
+  const redirect = useRedirect();
+  let classes: Array<ClassProps> = [];
+  const idMap = new Map<string, string>();
+  const onSubmit = (data: FieldValues) => {
+    const file: File = data['file'].rawFile;
+    // Read file data
+    file.text()
+      // Wait for file to read in as text
+      .then((text: string) => JSON.parse(text))
+      // Wait for JSON parsing into hopefully the right format.
+      // Capture current IDs in idMap.
+      .then((data: Array<ClassProps>) => {
+        classes = data;
+        data.map(item => idMap.set(item.id, ''));
+      })
+      // Create classes that have no parent_id. Associate new
+      // ID with old ID in idMap
+      .then(() => Promise.all(
+        classes.filter((item: ClassProps) => item.parent_id === '')
+        .map((item: ClassProps) => dataProvider.create('classes', { data: { ...item, id: '' } })
+          .then((result: CreateResult) => idMap.set(item.id, result.data.id))
+        )
+      ))
+      // Create classes with parent_ids, resetting old ID to new ID
+      .then(() => Promise.all(
+        classes.filter((item: ClassProps) => item.parent_id !== '')
+        .map((item: ClassProps) => {
+          const params = {
+            data: {
+              ...item,
+              id: '',
+              parent_id: idMap.get(item.parent_id),
+            },
+          };
+          return dataProvider.create('classes', params)
+        })
+      ))
+      // Pull class list and update side navigation
+      .then(() => props.update((new Date()).getTime()))
+      // We made it! Back to the list page.
+      .then(() => redirect('list', 'classes'));
+  };
+
+  return (
+    <Card sx={{ marginTop: '1em' }}>
+      <Title title="Class Import" />
+      <CardContent>
+        <SimpleForm onSubmit={onSubmit}>
+          <FileInput source="file" accept="application/json">
+            <FileField source="src" title="title" />
+          </FileInput>
+        </SimpleForm>
+      </CardContent>
+    </Card>
+  )
+}
