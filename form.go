e1cb15eca1a59eb79bbbd1ef53863e3058be0b85
jake <bigjake2s@gmail.com>
2022-09-20 18:37:03 -0400

    Filling in more FormService methods

--- form.go
+++ form.go
@@ -49,5 +49,34 @@ func (s FormService) ById(ctx context.Context, id string) (Form, error) {
 }
 
 func (s FormService) Create(ctx context.Context, form *Form) (err error) {
+	if form.Id != "" {
+		return fmt.Errorf("form already has an ID")
+	}
 
+	now := time.Now()
+	form.Id = idProvider.NewWithTime(now)
+	form.Created = now
+	form.Updated = now
+
+	return s.repo.CreateForm(ctx, form)
+}
+
+func (s FormService) Delete(ctx context.Context, id string) (err error) {
+	if !idProvider.IsValid(id) {
+		return fmt.Errorf("invalid form ID: %s", id)
+	}
+
+	return s.repo.DeleteForm(ctx, id)
+}
+
+func (s FormService) List(ctx context.Context, filter FormFilter) ([]Form, Range, error) {
+	return s.repo.GetFormList(ctx, filter)
+}
+
+func (s FormService) Update(ctx context.Context, form *Form) (err error) {
+	if !idProvider.IsValid(form.Id) {
+		return fmt.Errorf("invalid form ID: %s", form.Id)
+	}
+	form.Updated = time.Now()
+	return s.repo.UpdateForm(ctx, form)
 }
