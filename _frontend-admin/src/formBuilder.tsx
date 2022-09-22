6788e181c50a79d20d6552b029768e33808b4951
jake <bigjake2s@gmail.com>
2022-09-22 15:31:16 -0400

    Removing all the debugging

--- _frontend-admin/src/formBuilder.tsx
+++ _frontend-admin/src/formBuilder.tsx
@@ -36,16 +36,7 @@ export const FormBuilderInput = (props: CommonInputProps) => {
       <FieldTitle label={label} source={source} resource={resource} />
       <FormBuilder
         onChange={(schema: any) => field.onChange(schema)}
-        onUpdateComponent={(component: any, something: any, schema: any) => console.log('*** onUpdateComponent ***', 'component', component, 'something', something, 'schema', schema)}
-        onSaveComponent={(component: any, something: any, schema: any) => console.log('*** onSaveComponent ***', 'component', component, 'something', something, 'schema', schema)}
-        onEditComponent={(component: any, something: any, schema: any) => console.log('*** onEditComponent ***', 'component', component, 'something', something, 'schema', schema)}
-        onDeleteComponent={(component: any, something: any, schema: any) => console.log('*** onDeleteComponent ***', 'component', component, 'something', something, 'schema', schema)}
-        onCancelComponent={(component: any, something: any, schema: any) => console.log('*** onCancelComponent ***', 'component', component, 'something', something, 'schema', schema)}
         form={field.value}
-        options={{
-          onchange: (newValue: any) => console.log('onchange', newValue),
-          onChange: (one: any, two: any, three: any) => console.log('onChange', one, two, three),
-        }}
       />
     </div>
   )
