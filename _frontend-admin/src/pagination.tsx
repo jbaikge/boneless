bc26de66d0f0e19c7dab86586cdf587d03d8d12c
jake <bigjake2s@gmail.com>
2022-09-26 11:00:24 -0400

    Finally figured out setting default per-page

--- _frontend-admin/src/pagination.tsx
+++ _frontend-admin/src/pagination.tsx
@@ -1,3 +1,3 @@
 import { Pagination } from "react-admin";
 
-export const GlobalPagination = () => <Pagination rowsPerPageOptions={[ 25, 50, 100 ]} />;
+export const GlobalPagination = () => <Pagination perPage={50} rowsPerPageOptions={[ 25, 50, 100 ]} />;
