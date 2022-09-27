2c8b1f01ee7d1a264a13a4f877de4f1bee9dc77c
jake <bigjake2s@gmail.com>
2022-09-27 15:23:05 -0400

    `dynamoDocument` conversion functions

--- repositories/dynamodb/document.go
+++ repositories/dynamodb/document.go
@@ -3,6 +3,7 @@ package dynamodb
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/jbaikge/boneless"
 )
@@ -12,6 +13,57 @@ const documentPrefix = "doc#"
 func dynamoDocumentIds(id string, version int) (pk string, sk string) {
 	pk = documentPrefix + id
 	sk = documentPrefix + fmt.Sprintf("v%06d", version)
+	return
+}
+
+type dynamoDocument struct {
+	PK         string
+	SK         string
+	ClassId    string
+	ParentId   string
+	TemplateId string
+	Version    int
+	Path       string
+	Created    time.Time
+	Updated    time.Time
+	Data       map[string]interface{}
+}
+
+func newDynamoDocument(doc *boneless.Document) (dyn *dynamoDocument) {
+	pk, sk := dynamoDocumentIds(doc.Id, doc.Version)
+	dyn = &dynamoDocument{
+		PK:         pk,
+		SK:         sk,
+		ClassId:    doc.ClassId,
+		ParentId:   doc.ParentId,
+		TemplateId: doc.TemplateId,
+		Version:    doc.Version,
+		Path:       doc.Path,
+		Created:    doc.Created,
+		Updated:    doc.Updated,
+		Data:       make(map[string]interface{}),
+	}
+	for k, v := range doc.Values {
+		dyn.Data[k] = v
+	}
+	return
+}
+
+func (dyn *dynamoDocument) ToDocument() (doc boneless.Document) {
+	doc = boneless.Document{
+		Id:         dyn.PK[len(documentPrefix):],
+		ClassId:    dyn.ClassId,
+		ParentId:   dyn.ParentId,
+		TemplateId: dyn.TemplateId,
+		Version:    dyn.Version,
+		Path:       dyn.Path,
+		Created:    dyn.Created,
+		Updated:    dyn.Updated,
+		Values:     make(map[string]interface{}),
+	}
+	for k, v := range dyn.Data {
+		doc.Values[k] = v
+	}
 	return
 }
 
