708175b75a97829d12087cd03b09ee3aa3829a80
jake <bigjake2s@gmail.com>
2022-09-29 11:38:29 -0400

    File upload and S3 PUT URL generator

--- repositories/dynamodb/file.go
+++ repositories/dynamodb/file.go
@@ -2,14 +2,55 @@ package dynamodb
 
 import (
 	"context"
+	"fmt"
+	"strings"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/jbaikge/boneless"
 )
 
-func (repo *DynamoDBRepository) CreateFile(context.Context, *boneless.File) (location string, err error) {
-	return
+func (repo *DynamoDBRepository) CreateFile(ctx context.Context, f *boneless.File) (location string, err error) {
+	path := fmt.Sprintf("%s/%s", time.Now().Format("2006/01/02"), f.Filename)
+	params := &s3.PutObjectInput{
+		Bucket:      &repo.resources.StaticBucket,
+		Key:         &path,
+		Body:        f.Data,
+		ContentType: &f.ContentType,
+	}
+	if _, err = repo.s3.PutObject(ctx, params); err != nil {
+		return
+	}
+
+	return fmt.Sprintf("https://%s/%s", repo.resources.StaticDomain, path), nil
 }
 
-func (repo *DynamoDBRepository) CreateUploadUrl(context.Context, boneless.FileUploadRequest) (response boneless.FileUploadResponse, err error) {
+func (repo *DynamoDBRepository) CreateUploadUrl(ctx context.Context, request boneless.FileUploadRequest) (response boneless.FileUploadResponse, err error) {
+	key := strings.TrimLeft(request.Key, "/")
+	params := &s3.PutObjectInput{
+		Bucket:      &repo.resources.StaticBucket,
+		Key:         &key,
+		ContentType: &request.ContentType,
+	}
+
+	expires, err := time.ParseDuration(request.Expires)
+	if err != nil {
+		err = fmt.Errorf("bad duration, %s: %w", request.Expires, err)
+		return
+	}
+	addDuration := func(po *s3.PresignOptions) {
+		po.Expires = expires
+	}
+
+	signed, err := s3.NewPresignClient(repo.s3).PresignPutObject(ctx, params, addDuration)
+	if err != nil {
+		return
+	}
+
+	response.URL = signed.URL
+	response.Method = signed.Method
+	response.Headers = signed.SignedHeader
+	response.Location = fmt.Sprintf("https://%s/%s", repo.resources.StaticDomain, key)
+
 	return
 }
