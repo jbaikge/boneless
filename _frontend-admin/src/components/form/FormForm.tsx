dae4a6e01f7676e9b93648430802acc2a7f00396
jake <bigjake2s@gmail.com>
2022-10-11 13:58:56 -0400

    Initial attempt at revamping the form generator

--- _frontend-admin/src/components/form/FormForm.tsx
+++ _frontend-admin/src/components/form/FormForm.tsx
@@ -1,14 +1,39 @@
 import {
-  SimpleForm,
+  ArrayInput,
+  BooleanInput,
+  FormTab,
+  NumberInput,
+  SelectInput,
+  SimpleFormIterator,
+  TabbedForm,
   TextInput,
+  required,
 } from 'react-admin';
 
 const FormForm = () => {
   return (
-    <SimpleForm>
-      <TextInput source="id" label="Form ID" helperText="Available after creation" disabled fullWidth />
-      <TextInput source="name" fullWidth />
-    </SimpleForm>
+    <TabbedForm>
+      <FormTab label="Settings">
+        <TextInput source="id" label="Form ID" helperText="Available after creation" disabled fullWidth />
+        <TextInput source="name" fullWidth />
+      </FormTab>
+      <FormTab label="Fields">
+        <ArrayInput source="fields">
+          <SimpleFormIterator className="field-row">
+            <TextInput source="label" validate={[ required('A label is required') ]} />
+            <TextInput source="name" validate={[ required('A name is required') ]} />
+            <BooleanInput source="required" defaultValue={false} label="Required" />
+            <NumberInput source="column" defaultValue={0} label="List Column (0 = hidden)" />
+            <SelectInput source="type" choices={[{ id: 'text', name: 'Text' }]} defaultValue="text" validate={[ required('A type is required') ]} />
+            <ArrayInput source="validations">
+              <SimpleFormIterator>
+                <SelectInput source="type" choices={[{id: 'required', name: 'Required'}]}></SelectInput>
+              </SimpleFormIterator>
+            </ArrayInput>
+          </SimpleFormIterator>
+        </ArrayInput>
+      </FormTab>
+    </TabbedForm>
   );
 };
 
