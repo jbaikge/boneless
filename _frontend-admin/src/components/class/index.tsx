c4525b3708d6d8e18e985647928f84cced42b4c4
jake <bigjake2s@gmail.com>
2022-10-10 20:08:10 -0400

    Fixing import paths

--- _frontend-admin/src/components/class/index.tsx
+++ _frontend-admin/src/components/class/index.tsx
@@ -1,4 +1,4 @@
 export { default as ClassCreate } from './ClassCreate';
-export { default as ClassEdit } from './ClassCreate';
-export { default as ClassImport } from './ClassCreate';
-export { default as ClassList } from './ClassCreate';
+export { default as ClassEdit } from './ClassEdit';
+export { default as ClassImport } from './ClassImport';
+export { default as ClassList } from './ClassList';
--- _frontend-admin/src/components/template/index.tsx
+++ _frontend-admin/src/components/template/index.tsx
@@ -1,4 +1,4 @@
 export { default as TemplateCreate } from './TemplateCreate';
-export { default as TemplateEdit } from './TemplateCreate';
-export { default as TemplateImport } from './TemplateCreate';
-export { default as TemplateList } from './TemplateCreate';
+export { default as TemplateEdit } from './TemplateEdit';
+export { default as TemplateImport } from './TemplateImport';
+export { default as TemplateList } from './TemplateList';
