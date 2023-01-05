9c3dec07c5c8ef1697ec366fd015a14c42bfa069
jake <bigjake2s@gmail.com>
2023-01-05 15:06:09 -0500

    Abstracting logger from classes

--- internal/classes/go.mod
+++ internal/classes/go.mod
@@ -5,9 +5,11 @@ go 1.19
 require (
 	github.com/jbaikge/boneless/internal/common v0.0.0-00010101000000-000000000000
 	github.com/zeebo/assert v1.3.1
-	golang.org/x/exp v0.0.0-20221230185412-738e83a70c30
 )
 
-require github.com/rs/xid v1.4.0 // indirect
+require (
+	github.com/rs/xid v1.4.0 // indirect
+	golang.org/x/exp v0.0.0-20221230185412-738e83a70c30 // indirect
+)
 
 replace github.com/jbaikge/boneless/internal/common => ../common/
--- internal/classes/service/application.go
+++ internal/classes/service/application.go
@@ -4,15 +4,15 @@ import (
 	"context"
 	"errors"
 
+	"github.com/jbaikge/boneless/internal/classes/adapters"
 	"github.com/jbaikge/boneless/internal/classes/app"
 	"github.com/jbaikge/boneless/internal/classes/app/command"
 	"github.com/jbaikge/boneless/internal/classes/domain/class"
 	"github.com/jbaikge/boneless/internal/common/logger"
 	"github.com/jbaikge/boneless/internal/common/storage"
-	"golang.org/x/exp/slog"
 )
 
-func NewApplication(ctx context.Context, store storage.Option) app.Application {
+func NewApplication(ctx context.Context, store storage.Option, log logger.Logger) app.Application {
 	var classRepo class.Repository
 	switch store {
 	case storage.DynamoDB:
@@ -20,14 +20,14 @@ func NewApplication(ctx context.Context, store storage.Option) app.Application {
 	case storage.SQLite:
 		// TODO
 	case storage.Memory:
-		// TODO
+		classRepo = adapters.NewClassMemoryRepository()
 	default:
-		slog.Error("unable to init class application", errors.New("invalid storage option"), "option", store)
+		log.Error("unable to init class application", errors.New("invalid storage option"), "option", store)
 	}
 
 	return app.Application{
 		Commands: app.Commands{
-			AddClass: command.NewAddClassHandler(classRepo, logger.Default),
+			AddClass: command.NewAddClassHandler(classRepo, log),
 		},
 	}
 }
